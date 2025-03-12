a9aaaa60e243b9ad8d9764bc6651a06c8bb9c22b
Andreas Skielboe <skielboe@gmail.com>
2025-03-12 09:00:17 +0100

    flake.nix

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "A nixvim configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixvim.url = "github:nix-community/nixvim";
+  };
+
+  outputs =
+    { nixpkgs, nixvim, ... }@inputs:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = fn: nixpkgs.lib.genAttrs systems fn;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          nixvim' = nixvim.legacyPackages.${system};
+          nixvimModule = {
+            inherit system;
+            module = import ./config;
+            extraSpecialArgs = { inherit inputs; };
+          };
+          nvim = nixvim'.makeNixvimWithModule nixvimModule;
+        in
+        {
+          default = nvim;
+        }
+      );
+
+      checks = forAllSystems (
+        system:
+        let
+          nixvimLib = nixvim.lib.${system};
+          nixvimModule = {
+            inherit system;
+            module = import ./config;
+            extraSpecialArgs = { inherit inputs; };
+          };
+        in
+        {
+          default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
+        }
+      );
+    };
+}
